e9f27b4c84c89e3911530bd93539f00b6334b2fa
parthmern <parthmern@gmail.com>
2024-12-19 15:03:34 -0500

    hono initalize

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { Hono } from 'hono'
 
-const app = new Hono<{ Bindings: CloudflareBindings }>()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+  }
+}>()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  const DATABASE_URL = c.env.DATABASE_URL || "";
+  console.log(DATABASE_URL);
+  return c.text('hello from hono js with cloudflare workers/wrangler');
+
 })
 
 export default app
